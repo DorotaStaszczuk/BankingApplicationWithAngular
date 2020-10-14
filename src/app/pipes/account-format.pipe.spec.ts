5eb9e01a68b611387d5759a3bcd97d99755a3a9a
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2020-10-14 21:57:51 +0200

    Auto-generated commit

--- src/app/pipes/account-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AccountFormatPipe } from './account-format.pipe';
-
-describe('AccountFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AccountFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
